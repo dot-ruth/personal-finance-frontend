a5dc5a11a7aa4662e2b58836103dc6e1703a9874
dot-ruth <ruthgetaneh5@gmail.com>
2024-07-18 11:13:28 +0300

    FEAT|#2|added Landing page

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -20,10 +22,12 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('personal-finance');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, personal-finance');
-  });
+  it(`should display the landing page`,()=>{
+    let router: Router = TestBed.inject(Router);
+    let location: Location = TestBed.inject(Location);
+    router.navigate(['']);
+    expect(location.path()).toBe('');
+  })
+
+  
 });
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component'
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:LandingPageComponent}
+];
