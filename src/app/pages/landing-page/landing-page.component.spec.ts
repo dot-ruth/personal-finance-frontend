de6496da13d973bcb325625bf65d6b440abefa1e
dot-ruth <ruthgetaneh5@gmail.com>
2024-07-18 12:15:59 +0300

    FEAT|#2|added login and register page

--- src/app/pages/landing-page/landing-page.component.spec.ts
+++ src/app/pages/landing-page/landing-page.component.spec.ts
@@ -1,12 +1,16 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { LandingPageComponent } from './landing-page.component';
 import { By } from '@angular/platform-browser';
 import { Location } from '@angular/common';
+import { DebugElement } from '@angular/core';
 
 describe('LandingPageComponent', () => {
   let component: LandingPageComponent;
   let fixture: ComponentFixture<LandingPageComponent>;
-  let location:Location = TestBed.inject(Location)
+  let location:Location;
+  let loginButton:DebugElement;
+  let registerButton:DebugElement;
+  
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,22 +21,25 @@ describe('LandingPageComponent', () => {
     fixture = TestBed.createComponent(LandingPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    location = TestBed.inject(Location);
+    loginButton = fixture.debugElement.query(By.css('#loginButton')); 
+    registerButton = fixture.debugElement.query(By.css('#registerButton'));
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('redirect to login', () => {
-  let button = fixture.debugElement.query(By.css('#loginButton')).nativeElement;
-  button.click();
-  expect(location.path()).toBe('/login');
-  })
+  it('should redirect to login',fakeAsync( () => {
+  spyOn(component, 'toLogin')
+  loginButton.nativeElement.click();
+  expect(component.toLogin).toHaveBeenCalled();
+  }))
 
-  it('redirect to registration', () => {
-    let button = fixture.debugElement.query(By.css('#registerButton')).nativeElement;
-    button.click();
-    expect(location.path()).toBe('/register');
+  it('should redirect to registration', () => {
+    spyOn(component, 'toRegister')
+    registerButton.nativeElement.click();
+    expect(component.toRegister).toHaveBeenCalled();
    
   })
 });
