d8374186d0b5ee365a00c899c6574c213450d10d
dot-ruth <ruthgetaneh5@gmail.com>
2024-07-18 16:50:14 +0300

    TASK|#1|added registratio form to the registration page

--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.scss'
 })
 export class RegisterPageComponent {
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
 
+  Register() { }
 }
