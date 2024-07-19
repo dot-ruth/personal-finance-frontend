ca44f2f6d2e9ef3894bdffd6fa1b55f882b27438
dot-ruth <ruthgetaneh5@gmail.com>
2024-07-19 14:59:35 +0300

    CLEANUP|#1|code cleanup

--- src/app/pages/landing-page/landing-page.component.spec.ts
+++ src/app/pages/landing-page/landing-page.component.spec.ts
@@ -1,13 +1,11 @@
-import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
 import { LandingPageComponent } from './landing-page.component';
 import { By } from '@angular/platform-browser';
-import { Location } from '@angular/common';
 import { DebugElement } from '@angular/core';
 
 describe('LandingPageComponent', () => {
   let component: LandingPageComponent;
   let fixture: ComponentFixture<LandingPageComponent>;
-  let location:Location;
   let loginButton:DebugElement;
   let registerButton:DebugElement;
   
@@ -21,7 +19,6 @@ describe('LandingPageComponent', () => {
     fixture = TestBed.createComponent(LandingPageComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-    location = TestBed.inject(Location);
     loginButton = fixture.debugElement.query(By.css('#loginButton')); 
     registerButton = fixture.debugElement.query(By.css('#registerButton'));
   });
