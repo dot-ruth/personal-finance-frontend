72af7618d0322f3878adff338c6b0fe7cf63c85b
dot-ruth <ruthgetaneh5@gmail.com>
2024-07-19 12:10:16 +0300

    FIX|#1|implemented error handing with form group

--- src/app/pages/register-page/register-page.component.ts
+++ src/app/pages/register-page/register-page.component.ts
@@ -1,60 +1,37 @@
 import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
-import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule, FormsModule } from '@angular/forms';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
-import { merge } from 'rxjs';
+
 
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
+  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule, ReactiveFormsModule, FormsModule],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
+ 
 export class RegisterPageComponent {
- readonly email = new FormControl('',[
-  Validators.required,
-  Validators.email
- ])
 
- readonly password = new FormControl('',[Validators.required ])
+  registrationForm = new FormGroup ({
+     email : new FormControl('',[Validators.required,Validators.email]),
+     password : new FormControl('',[Validators.required, Validators.minLength(8), Validators.maxLength(20) ]),
+     confirmpassword : new FormControl('', [Validators.required,Validators.minLength(8), Validators.maxLength(20)])
+  })
 
- readonly confirmpassword = new FormControl('', [Validators.required])
-
- errorMessage = signal('')
-
- constructor( ) {
-  merge(this.email.statusChanges, this.email.valueChanges)
-  .pipe(takeUntilDestroyed())
-  .subscribe(()=>this.updateErrorMessage())
- }
-
- updateErrorMessage(){
-  if(this.email.hasError('required')){
-    this.errorMessage.set('You Must enter a value');
-  }else if ( this.email.hasError('email')){
-    this.errorMessage.set('Not a Valid Email');
-  }else if ( this.password.hasError('required')){
-    this.errorMessage.set('Please enter password')
-  }else if ( this.confirmpassword.hasError('required') ){
-    this.errorMessage.set('Please confirm your password')
-  }else if( this.password !== this.confirmpassword) {
-    this.errorMessage.set('Confirm password doesnot match you password')
-  }else{
-    this.errorMessage.set('')
+  Register() {
+    console.log("form submitted")
   }
- }
-
+ 
   hide = signal(true);
   clickEvent(event: MouseEvent) {
     this.hide.set(!this.hide());
     event.stopPropagation();
   }
 
-  Register() { }
 }
