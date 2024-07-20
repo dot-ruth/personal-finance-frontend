fdaff7650a190f94cfe7edeea4d1efdad043d22e
dot-ruth <ruthgetaneh5@gmail.com>
2024-07-20 11:31:30 +0300

    FEAT|#7|added sidenav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet,Router,NavigationStart } from '@angular/router';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,SidenavComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'personal-finance';
+
+  showMenu:boolean|undefined
+
+
+  constructor(router:Router) {
+    router.events.forEach((event) => {
+        if(event instanceof NavigationStart) {
+            this.showMenu = (event.url !== "/") && 
+            (event.url !== '/login') && 
+            (event.url !== '/register') ;
+        }
+      
+      });
+    }
 }
---
+++ src/app/components/sidenav/sidenav.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { ProfilePictureComponent } from '../profile-picture/profile-picture.component';
+import { RouterOutlet,RouterLink } from '@angular/router';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatIcon } from '@angular/material/icon';
+import { MatListModule} from '@angular/material/list'
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import {
+   faHome,
+   faMoneyBill,
+   faLineChart,
+   faPieChart,
+   faBarChart,
+   faUser,
+   faSignOut,
+   faSign
+   } 
+from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-sidenav',
+  standalone: true,
+  imports: [ProfilePictureComponent,RouterOutlet,MatSidenavModule,FontAwesomeModule,MatIcon,RouterLink,MatListModule],
+  templateUrl: './sidenav.component.html',
+  styleUrl: './sidenav.component.scss'
+})
+export class SidenavComponent {
+
+  faHome = faHome;
+  faMoneyBill = faMoneyBill;
+  faLineChart = faLineChart;
+  faPieChart = faPieChart;
+  faBarChart = faBarChart;
+  faUser = faUser;
+  faSignOut = faSignOut;
+  selectedItem:string | undefined;
+  profile_url!: string;
+
+  onNavigationClick(item: string) {
+    this.selectedItem = item;
+  }
+
+  logout() {
+
+  }
+
+}
