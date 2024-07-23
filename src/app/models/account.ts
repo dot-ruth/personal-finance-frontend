d2c6191d460a77ed475a681551306de14eeac6a8
dot-ruth <ruthgetaneh5@gmail.com>
2024-07-23 12:10:15 +0300

    TASK|#11| added account model and CRUD functionality to the account model

---
+++ src/app/models/account.ts
@@ -0,0 +1,6 @@
+export interface Account {
+    id:number;
+    name: string;
+    color: string;
+    amount: number;
+}
---
+++ src/app/services/account-services.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Account } from '../models/account';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountServicesService {
+
+  constructor() { }
+  private account!:Account[]
+
+  createAccount ( account: Account): void{
+    this.account.push(account)
+  }
+
+  getAccount (): Account[] {
+    return this.account;
+  }
+
+  getAccountById(id:number):Account | undefined{
+    return this.account.find(acc => acc.id === id)
+  }
+
+  updateAccount ( updatedAccount: Account ):void {
+    const index = this.account.findIndex( acc => acc.id === updatedAccount.id)
+    this.account[index] = updatedAccount
+  }
+
+  delete (id:number):void {
+    this.account = this.account.filter(acc => acc.id !== id)
+  }
+}
